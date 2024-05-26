8de8dbddf5ea573f85bcc3eedb6019b21372ba1c
Mangdi <lkw4670@naver.com>
2024-05-26 23:40:06 +0900

    feat: FeedController에 UICollectionViewDataSource, UICollectionViewDelegateFlowLayout 관련 코드 구현

--- Instagram/Instagram/Controller/FeedController.swift
+++ Instagram/Instagram/Controller/FeedController.swift
@@ -7,12 +7,44 @@
 
 import UIKit
 
-final class FeedController: UIViewController {
+private let reuseIdentifier = "Cell"
+
+final class FeedController: UICollectionViewController {
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground
+        configureUI()
     }
     
+    // MARK: - Helpers
+    
+    private func configureUI() {
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension FeedController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
 }
