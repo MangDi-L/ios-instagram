cfb5e0ddcc8f5d1879b577444966759da50b5ba6
Mangdi <lkw4670@naver.com>
2024-05-27 02:10:13 +0900

    feat: 프로필이미지뷰, 사용자이름버튼 UI 추가 및 구현

--- Instagram/Instagram/View/FeedCell.swift
+++ Instagram/Instagram/View/FeedCell.swift
@@ -11,12 +11,38 @@ final class FeedCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        imageView.image = #imageLiteral(resourceName: "venom-7")
+        return imageView
+    }()
+    
+    private lazy var usernameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("venom", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .systemPurple
+        backgroundColor = .systemBackground
+        
+        [profileImageView, usernameButton].forEach { self.addSubview($0) }
+        
+        profileImageView.anchor(top: self.topAnchor, left: self.leftAnchor, paddingTop: 12, paddingLeft: 12, width: 40, height: 40)
+        profileImageView.layer.cornerRadius = 40 / 2
+        
+        usernameButton.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+        
     }
     
     required init?(coder: NSCoder) {
@@ -26,5 +52,13 @@ final class FeedCell: UICollectionViewCell {
     override func prepareForReuse() {
         super.prepareForReuse()
     }
+}
+
+// MARK: - Actions
+
+extension FeedCell {
     
+    @objc private func didTapUsername() {
+        
+    }
 }
