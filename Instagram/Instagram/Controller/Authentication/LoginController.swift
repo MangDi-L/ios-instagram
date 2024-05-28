00e01c6ce8dc0f5a27c6ee7bf50dfc69e8453459
Mangdi <lkw4670@naver.com>
2024-05-28 16:56:48 +0900

    feat: LoginController의 배경에 그라데이션효과주기

--- Instagram/Instagram/Controller/Authentication/LoginController.swift
+++ Instagram/Instagram/Controller/Authentication/LoginController.swift
@@ -16,7 +16,20 @@ final class LoginController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemMint
+        configureUI()
     }
     
+    // MARK: - Helpers
+    
+    private func configureUI() {
+        view.backgroundColor = .systemMint
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemBlue.cgColor]
+        gradient.locations = [0, 1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+    }
 }
