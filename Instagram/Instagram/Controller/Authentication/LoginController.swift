493a46b6d10192800e7cc362deb195f06a8eb373
Mangdi <lkw4670@naver.com>
2024-05-28 16:44:06 +0900

    feat: LoginController, RegistrationController 생성

---
+++ Instagram/Instagram/Controller/Authentication/LoginController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginController.swift
+//  Instagram
+//
+//  Created by mangdi on 5/28/24.
+//
+
+import UIKit
+
+final class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemMint
+    }
+    
+}
---
+++ Instagram/Instagram/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,22 @@
+//
+//  RegistrationController.swift
+//  Instagram
+//
+//  Created by mangdi on 5/28/24.
+//
+
+import UIKit
+
+final class RegistrationController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemMint
+    }
+    
+}
