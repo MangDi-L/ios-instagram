74a2d69ac6dfdcbe8e594c2bfead90a37b0ceacf
Mangdi <lkw4670@naver.com>
2024-05-29 00:15:56 +0900

    feat: 로그인화면의 아이콘이미지, 이메일, 패스워드 UI요소 추가 및 구현

--- Instagram/Instagram/Controller/Authentication/LoginController.swift
+++ Instagram/Instagram/Controller/Authentication/LoginController.swift
@@ -11,6 +11,39 @@ final class LoginController: UIViewController {
     
     // MARK: - Properties
     
+    private let iconImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "Instagram_logo_white")
+        return imageView
+    }()
+
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.textColor = .white
+        textField.keyboardAppearance = .dark
+        textField.keyboardType = .emailAddress
+        textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        textField.setHeight(50)
+        textField.attributedPlaceholder = NSAttributedString(string: "Email",
+                                                             attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.textColor = .white
+        textField.keyboardAppearance = .dark
+        textField.keyboardType = .emailAddress
+        textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        textField.setHeight(50)
+        textField.attributedPlaceholder = NSAttributedString(string: "Password",
+                                                             attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -31,5 +64,19 @@ final class LoginController: UIViewController {
         gradient.locations = [0, 1]
         view.layer.addSublayer(gradient)
         gradient.frame = view.frame
+        
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.setDimensions(height: 80, width: 120)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField])
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 32,
+                     paddingLeft: 32, paddingRight: 32)
     }
 }
