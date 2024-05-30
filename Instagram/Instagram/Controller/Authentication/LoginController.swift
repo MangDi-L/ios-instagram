e743ddb9f7fdafc2971cbb7784e5a0e5e14d860e
Mangdi <lkw4670@naver.com>
2024-05-30 23:23:51 +0900

    feat: CustomButton 파일 추가 및 구현, 등록화면 UI구현

--- Instagram/Instagram/Controller/Authentication/LoginController.swift
+++ Instagram/Instagram/Controller/Authentication/LoginController.swift
@@ -28,16 +28,7 @@ final class LoginController: UIViewController {
         return textField
     }()
     
-    private let loginButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Log In", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
-        button.layer.cornerRadius = 5
-        button.setHeight(50)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-        return button
-    }()
+    private let loginButton = CustomButton(buttonName: "Log In")
     
     private let forgotPasswordButton: UIButton = {
         let button = UIButton(type: .system)
--- Instagram/Instagram/Controller/Authentication/RegistrationController.swift
+++ Instagram/Instagram/Controller/Authentication/RegistrationController.swift
@@ -11,6 +11,29 @@ final class RegistrationController: UIViewController {
 
     // MARK: - Properties
     
+    private let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "plus_photo"), for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+    
+    private let emailTextField: CustomTextField = {
+        let textField = CustomTextField(placeholder: "Email")
+        return textField
+    }()
+    
+    private let passwordTextField: CustomTextField = {
+        let textField = CustomTextField(placeholder: "Password")
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    private let fullnameTextField = CustomTextField(placeholder: "Fullname")
+    private let usernameTextField = CustomTextField(placeholder: "Username")
+    
+    private let signUpButton = CustomButton(buttonName: "Sign Up")
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -23,5 +46,10 @@ final class RegistrationController: UIViewController {
     
     private func configureUI() {
         configureGradientLayer()
+        
+        view.addSubview(plusPhotoButton)
+        plusPhotoButton.centerX(inView: view)
+        plusPhotoButton.setDimensions(height: 140, width: 140)
+        plusPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
     }
 }
