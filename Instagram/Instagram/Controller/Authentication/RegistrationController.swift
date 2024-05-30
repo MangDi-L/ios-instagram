4960c2b83c2e8da9a2c8654fe05d72da0c83bc62
Mangdi <lkw4670@naver.com>
2024-05-30 23:36:37 +0900

    feat: 계정등록화면 UI구현 완료

--- Instagram/Instagram/Controller/Authentication/RegistrationController.swift
+++ Instagram/Instagram/Controller/Authentication/RegistrationController.swift
@@ -34,12 +34,25 @@ final class RegistrationController: UIViewController {
     
     private let signUpButton = CustomButton(buttonName: "Sign Up")
     
+    private let alreadyHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "Already have an account?  ", secondPart: "Log In")
+        button.addTarget(self, action: #selector(handleShowLogin), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func handleShowLogin() {
+        navigationController?.popViewController(animated: true)
     }
     
     // MARK: - Helpers
@@ -51,5 +64,20 @@ final class RegistrationController: UIViewController {
         plusPhotoButton.centerX(inView: view)
         plusPhotoButton.setDimensions(height: 140, width: 140)
         plusPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField,
+                                                   fullnameTextField, usernameTextField,
+                                                   signUpButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        view.addSubview(stack)
+        stack.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 32,
+                     paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(alreadyHaveAccountButton)
+        alreadyHaveAccountButton.centerX(inView: view)
+        alreadyHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
     }
 }
