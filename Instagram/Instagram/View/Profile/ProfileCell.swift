3d7f55568ebac983a32d3deed6b5491052fb13df
Mangdi <lkw4670@naver.com>
2024-06-05 18:37:15 +0900

    feat: ProfileCell 구현

--- Instagram/Instagram/View/Profile/ProfileCell.swift
+++ Instagram/Instagram/View/Profile/ProfileCell.swift
@@ -11,12 +11,22 @@ final class ProfileCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    private let postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "venom-7")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .systemBackground
+        addSubview(postImageView)
+        postImageView.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
