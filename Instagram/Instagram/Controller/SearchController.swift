25c75f887e9734a5a0511d4cd6c4f703ca4fabcc
Mangdi <lkw4670@naver.com>
2024-06-09 16:08:30 +0900

    feat: SearchController 테이블뷰에 UserCell 등록

--- Instagram/Instagram/Controller/SearchController.swift
+++ Instagram/Instagram/Controller/SearchController.swift
@@ -7,10 +7,38 @@
 
 import UIKit
 
-final class SearchController: UIViewController {
+final class SearchController: UITableViewController {
+    
+    // MARK: - Properties
+    private let reuseIdentifier = "UserCell"
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureTableView()
     }
     
+    // MARK: - Helpers
+    
+    private func configureTableView() {
+        view.backgroundColor = .systemBackground
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 64
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SearchController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
 }
