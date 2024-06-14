ecdeb154c1db363af376a068493c0273eeeb8f09
Mangdi <lkw4670@naver.com>
2024-06-14 16:23:58 +0900

    feat: 게시글을 Post할동안 Loading 화면이 보이도록 구현

--- Instagram/Instagram/Utils/Extentions.swift
+++ Instagram/Instagram/Utils/Extentions.swift
@@ -7,29 +7,21 @@
 //
 
 import UIKit
-//import JGProgressHUD
+import JGProgressHUD
 
 extension UIViewController {
-//    static let hud = JGProgressHUD(style: .dark)
-//    
-//    func configureGradientLayer() {
-//        let gradient = CAGradientLayer()
-//        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemBlue.cgColor]
-//        gradient.locations = [0, 1]
-//        view.layer.addSublayer(gradient)
-//        gradient.frame = view.frame
-//    }
-//    
-//    func showLoader(_ show: Bool) {
-//        view.endEditing(true)
-//        
-//        if show {
-//            UIViewController.hud.show(in: view)
-//        } else {
-//            UIViewController.hud.dismiss()
-//        }
-//    }
-//    
+    static let hud = JGProgressHUD(style: .dark)
+    
+    func showLoader(_ show: Bool) {
+        view.endEditing(true)
+        
+        if show {
+            UIViewController.hud.show(in: view)
+        } else {
+            UIViewController.hud.dismiss()
+        }
+    }
+    
 //    func showMessage(withTitle title: String, message: String) {
 //        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
 //        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
