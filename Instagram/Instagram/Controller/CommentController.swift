4a06ae214f47deda10b4886c67e183b14c5a6312
Mangdi <lkw4670@naver.com>
2024-06-19 00:48:20 +0900

    feat: CommentController UI구현 및 화면이 뜨고 사라질때 탭바 숨기고 보여주도록 구현

--- Instagram/Instagram/Controller/CommentController.swift
+++ Instagram/Instagram/Controller/CommentController.swift
@@ -13,6 +13,13 @@ final class CommentController: UICollectionViewController {
     
     // MARK: - Properties
     
+    private lazy var commtentInputView: CommentInputAccessoryView = {
+        let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
+        let view = CommentInputAccessoryView(frame: frame)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -20,11 +27,34 @@ final class CommentController: UICollectionViewController {
 
         configureCollectionView()
     }
+    
+    // 실험
+//    override var inputAccessoryView: UIView? {
+//        get { return commtentInputView }
+//    }
+    
+    // 실험
+//    override var canBecomeFirstResponder: Bool {
+//        return true
+//    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        tabBarController?.tabBar.isHidden = false
+    }
 
     // MARK: - Helpers
     
     private func configureCollectionView() {
         collectionView.backgroundColor = .systemBackground
+        navigationItem.title = "Comments"
         self.collectionView.register(CommentCell.self, forCellWithReuseIdentifier: reuseIdentifier)
     }
 }
@@ -38,7 +68,6 @@ extension CommentController {
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        cell.backgroundColor = .red
     
         return cell
     }
