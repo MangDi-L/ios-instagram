39f44aa1625b8a0e7e3f8b2f32492a2728d35fce
Mangdi <lkw4670@naver.com>
2024-06-24 15:10:52 +0900

    feat: NotificationCell 추가 및 UI구현, NotificationController UI구현

--- Instagram/Instagram/Controller/NotificationController.swift
+++ Instagram/Instagram/Controller/NotificationController.swift
@@ -7,10 +7,41 @@
 
 import UIKit
 
-final class NotificationController: UIViewController {
+private let reuseIdentifier = "NotificationCell"
+
+final class NotificationController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureTableView()
     }
     
+    // MARK: - Helpers
+    
+    private func configureTableView() {
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Notification"
+        
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 80
+        tableView.separatorStyle = .none
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension NotificationController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? NotificationCell ?? NotificationCell()
+        return cell
+    }
 }
