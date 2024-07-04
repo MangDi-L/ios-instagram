9591571efe9cf7d31c80793dd3eb0c06ec135645
Mangdi <lkw4670@naver.com>
2024-07-04 19:57:34 +0900

    feat: 가입할때 fullname, username중복시 가입거부되도록 구현

--- Instagram/Instagram/API/AuthService.swift
+++ Instagram/Instagram/API/AuthService.swift
@@ -8,6 +8,11 @@
 import UIKit
 import Firebase
 
+enum AuthServiceError: String, Error {
+    case fullnameDuplicate = "The fullname is already in use by another account."
+    case usernameDuplicate = "The username is already in use by another account."
+}
+
 struct AuthCredentials {
     let email: String
     let password: String
@@ -22,30 +27,58 @@ struct AuthService {
     }
     
     static func registerUser(withCredential credentials: AuthCredentials, completion: @escaping (Result<(), Error>) -> Void) {
-        ImageUploader.uploadImage(image: credentials.profileImage) { imageUrl in
-            Auth.auth().createUser(withEmail: credentials.email, password: credentials.password) { authDataResult, error in
-                if let error = error {
-                    print("DEBUG: Failed to register user \(error.localizedDescription)")
-                    completion(.failure(error))
-                    return
+        checkupNamesDuplicate(fullname: credentials.fullname, username: credentials.username) { result in
+            switch result {
+            case .success:
+                ImageUploader.uploadImage(image: credentials.profileImage) { imageUrl in
+                    Auth.auth().createUser(withEmail: credentials.email, password: credentials.password) { authDataResult, error in
+                        if let error = error {
+                            print("DEBUG: Failed to register user \(error.localizedDescription)")
+                            completion(.failure(error))
+                            return
+                        }
+                        
+                        guard let uid = authDataResult?.user.uid else { return }
+                        
+                        let data: [String: Any] = ["email": credentials.email,
+                                                   "fullname": credentials.fullname,
+                                                   "profileImageUrl": imageUrl,
+                                                   "uid": uid,
+                                                   "username": credentials.username]
+                        
+                        COLLECTION_USERS.document(uid).setData(data) { error in
+                            completion(.success(()))
+                        }
+                    }
                 }
-                
-                guard let uid = authDataResult?.user.uid else { return }
-                
-                let data: [String: Any] = ["email": credentials.email,
-                                           "fullname": credentials.fullname,
-                                           "profileImageUrl": imageUrl,
-                                           "uid": uid,
-                                           "username": credentials.username]
-                
-                COLLECTION_USERS.document(uid).setData(data) { error in
-                    completion(.success(()))
-                }
+            case .failure(let failure):
+                completion(.failure(failure))
             }
         }
     }
     
     static func resetPassword(withEmail email: String, completion: @escaping (Error?) -> Void) {
-        Auth.auth().sendPasswordReset(withEmail: email, completion: completion)
+        Auth.auth().sendPasswordReset (withEmail: email, completion: completion)
+    }
+    
+    static func checkupNamesDuplicate(fullname: String, username: String, completion: @escaping (Result<Void, AuthServiceError>) -> Void) {
+        COLLECTION_USERS.getDocuments { querySnapshot, error in
+            guard let snapshot = querySnapshot else { return }
+            
+            if snapshot.documents.isEmpty { completion(.success(())) }
+            
+            let users = snapshot.documents.map { User(dictionary: $0.data()) }
+            let fullnames = users.map { $0.fullname }
+            let usernames = users.map { $0.username }
+            
+            if fullnames.contains(fullname) {
+                completion(.failure(.fullnameDuplicate))
+            } else if usernames.contains(username) {
+                completion(.failure(.usernameDuplicate))
+            } else {
+                completion(.success(()))
+            }
+            
+        }
     }
 }
